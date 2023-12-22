80715a63a630255d4c7963cbcc71eabf151702ba
rryta <rryta@MMB-W-WIN10>
2023-12-22 21:10:16 +0300

    Добавьте файлы проекта.

---
+++ lab3/Composition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Data.SQLite;
+using System;
+using System.Collections.Generic;
+
+
+public class Composition
+{
+	public string author { get; set; }
+    public string song { get; set; }
+    public Composition()
+    {
+        author = string.Empty;
+        song = string.Empty;
+    }
+    public Composition(string _author, string _song)
+	{
+        author= _author;
+		song = _song;
+    }
+    public override string ToString()
+	{
+		return author + " - " + song;
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace SerializeToFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            Catalog myCatalog = new Catalog();
+            Catalog.DisplayHelp();
+
+            while (true)
+            {
+                string request = myCatalog.getRequest();
+                Console.Write(request);
+                string response = Console.ReadLine().ToLower();
+                if (response == "quit")
+                    break;
+                Console.Write(myCatalog.setResponse(response));
+            }
+        }
+    }
+}
